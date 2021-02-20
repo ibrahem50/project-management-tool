ee8455e55b08385d97e797e346d3c84be8e6f23a
ibrahem <ibrahem.ahmed.ibrahem500@gmail.com>
2021-02-20 11:27:25 +0200

    signedIn component added

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,10 +1,12 @@
 import {Link} from 'react-router-dom';
+import SignedInLinks from './SignedInLinks';
 
 const Navbar = () => {
     return ( 
         <nav className="nav-wrapper grey darken-4">
             <div className="container">
                 <Link to='/'>OurPlan</Link>
+                <SignedInLinks />
             </div>
         </nav>
      );
---
+++ src/components/layout/SignedInLinks.js
@@ -0,0 +1,13 @@
+import {NavLink} from 'react-router-dom';
+
+const SignedInLinks = () => {
+    return ( 
+        <ul className="right">
+            <li><NavLink to = '/'>NewProject</NavLink></li>
+            <li><NavLink to = '/'>Log Out</NavLink></li>
+            <li><NavLink to = '/' className='btn btn-floating pink lighten-1'>HH</NavLink></li>
+        </ul>
+    );
+}
+ 
+export default SignedInLinks;
