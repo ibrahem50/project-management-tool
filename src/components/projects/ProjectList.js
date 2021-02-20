a2c666d27ba81d4a5be2800738ad02f5bb94348c
ibrahem <ibrahem.ahmed.ibrahem500@gmail.com>
2021-02-20 11:53:56 +0200

    ProjectSummary component added

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,29 +1,12 @@
+import ProjectSummary from "./ProjectSummary";
+
 const ProjectList = () => {
     return ( 
         <div className="project-list section">
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by Hema</p>
-                    <p className="grey-text">3rd sept , 2am</p>
-                </div>
-            </div>
-
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by Hema</p>
-                    <p className="grey-text">3rd sept , 2am</p>
-                </div>
-            </div>
-
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by Hema</p>
-                    <p className="grey-text">3rd sept , 2am</p>
-                </div>
-            </div>
+           <ProjectSummary />
+           <ProjectSummary />
+           <ProjectSummary />
+           <ProjectSummary />
         </div>
      );
 }
