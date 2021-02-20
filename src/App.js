9048e8cdc748c9ca86e2b12669bf970112393ddd
ibrahem <ibrahem.ahmed.ibrahem500@gmail.com>
2021-02-20 13:53:12 +0200

    Added style

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ function App() {
           <Route exact path='/' component = {Dashboard} />
           <Route path='/project/:id' component={ProjectDetails} />
         </Switch>
-
       </div>
     </BrowserRouter>
   );
