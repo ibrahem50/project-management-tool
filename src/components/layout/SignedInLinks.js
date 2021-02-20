2e91d602f6fd3b71b9d065839db18e96bde78d0d
ibrahem <ibrahem.ahmed.ibrahem500@gmail.com>
2021-02-20 14:43:52 +0200

    Added Nav links

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -3,7 +3,7 @@ import {NavLink} from 'react-router-dom';
 const SignedInLinks = () => {
     return ( 
         <ul className="right">
-            <li><NavLink to = '/'>NewProject</NavLink></li>
+            <li><NavLink to = '/create'>NewProject</NavLink></li>
             <li><NavLink to = '/'>Log Out</NavLink></li>
             <li><NavLink to = '/' className='btn btn-floating pink lighten-1'>HH</NavLink></li>
         </ul>
