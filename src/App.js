7b7951255934feccf6f4339bd19be8ce03bf81fa
ibrahem <ibrahem.ahmed.ibrahem500@gmail.com>
2021-02-20 14:41:16 +0200

    Added createprojects component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import SignIn from './components/auth/SignIn'
 import SignUp from './components/auth/SignUp'
 import Dashboard from './components/dashboard/Dashboard'
 import Navbar from './components/layout/Navbar'
+import CreateProject from './components/projects/CreateProject'
 import ProjectDetails from './components/projects/ProjectDetails'
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
           <Route path='/project/:id' component={ProjectDetails} />
           <Route path='/signin' component={SignIn} />
           <Route path='/signup' component={SignUp} />
+          <Route path='/create' component={CreateProject} />
         </Switch>
       </div>
     </BrowserRouter>
