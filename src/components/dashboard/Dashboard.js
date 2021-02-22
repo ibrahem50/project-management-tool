ec1bd47fba25db4ce7c240a3de33f50ee53bc447
ibrahem <ibrahem.ahmed.ibrahem500@gmail.com>
2021-02-22 11:28:46 +0200

    added dummy data and connected the app to redux

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import ProjectList from '../projects/ProjectList';
 import Notifications from './Notifications';
+import {connect} from 'react-redux';
 
 class Dashboard extends Component {
-    render() { 
+    render() {
+        const {projects} = this.props;
+        //console.log(this.props);
         return ( 
             <div className="dashboard container">
                 <div className="row">
                     <div className="col s12 m6">
-                        <ProjectList />
+                        <ProjectList projects={projects}/>
                     </div>
                     <div className="col s12 m5 offset-m1">
                         <Notifications />
@@ -18,5 +21,11 @@ class Dashboard extends Component {
          );
     }
 }
+
+const mapStateToProps = (state)=>{
+ return{
+     projects: state.project.projects
+ }
+}
  
-export default Dashboard;
+export default connect(mapStateToProps)(Dashboard);
--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,4 +1,10 @@
-const initialState = {}
+const initialState = {
+    projects : [
+        {id:'1', title: 'help mw find', content: 'blaaah blaah'},
+        {id:'2', title:'collect all', content: 'blaaah blaah'},
+        {id:'3', title:'egg hunt', content:'blah'}
+    ]
+}
 
 const projectReducer = (state = initialState , action) => {
     return state
