b236cf4829d32bfb1a6b9aaf9c87ad79f75441af
ibrahem <ibrahem.ahmed.ibrahem500@gmail.com>
2021-02-24 15:21:21 +0200

    added user signup firebaseAuth

--- src/store/actions/authActions.js
+++ src/store/actions/authActions.js
@@ -25,3 +25,25 @@ export const signOut = () => {
         });
     }
 }
+
+export const signUp = (newUser) => {
+    return (dispatsh,getState,{getFirebase,getFirestore}) => {
+        const firebase = getFirebase();
+        const firestore = getFirestore();
+
+        firebase.auth().createUserWithEmailAndPassword(
+            newUser.email,
+            newUser.password
+        ).then((res)=>{
+            return firestore.collection('users').doc(res.user.uid).set({
+                firstName: newUser.firstName,
+                lastName: newUser.lastName,
+                initials: newUser.firstName[0] + newUser.lastName[0]
+            })
+        }).then(()=>{
+            dispatsh({type: 'SIGNUP_SUCCESS'})
+        }).catch(err=>{
+            dispatsh({type:'SIGNUP_ERROR',err})
+        })
+    }
+}
--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -17,8 +17,21 @@ const authReducer = (state = initialState , action) => {
                 authError: null
             }
         case 'SIGNOUT_SUCCESS':
-            console.log('signout success')
+            console.log('signout success');
             return state;
+        case 'SIGNUP_SUCCESS':
+            console.log('signup success');
+            return{
+                ...state,
+                authError:null
+            }
+        case 'SIGNUP_Error':
+            console.log('signup error');
+            return{
+                ...state,
+                authError: action.err.message
+            }
+
         default: return state
     }
 } 
