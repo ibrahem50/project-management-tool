96b7b47ea77ac37a87fe9950a91c1e7fe71c1962
ibrahem <ibrahem.ahmed.ibrahem500@gmail.com>
2021-02-24 12:22:30 +0200

    added auth Reducer

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -1,7 +1,23 @@
-const initialState = {}
+const initialState = {
+    authError: null
+}
 
 const authReducer = (state = initialState , action) => {
-    return state
+    switch (action.type) {
+        case 'LOGIN_ERROR':
+            console.log('error');
+            return {
+                ...state,
+                authError: 'Login Failed'
+            }
+        case 'LOGIN_SUCCESS':
+            console.log('success');
+            return {
+                ...state,
+                authError: null
+            }
+        default: return state
+    }
 } 
 
 export default authReducer;
