238c53abf62f86381d9cf959bb3f2b0d28f677f3
ibrahem <ibrahem.ahmed.ibrahem500@gmail.com>
2021-02-24 14:32:05 +0200

    added waiting for auth ready

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import App from './App';
 import './index.css';
 import {createStore , applyMiddleware , compose} from 'redux';
 import rootReducer from './store/reducers/rootReducer';
-import {Provider} from 'react-redux';
+import {useSelector,Provider} from 'react-redux';
 import thunk from 'redux-thunk';
 import {reduxFirestore,getFirestore,createFirestoreInstance} from 'redux-firestore';
-import {ReactReduxFirebaseProvider,getFirebase} from 'react-redux-firebase';
+import {isLoaded,ReactReduxFirebaseProvider,getFirebase} from 'react-redux-firebase';
 import firebase from 'firebase/app';
 import firebaseConfig from './config/fbConfig';
 
@@ -21,14 +21,28 @@ const rrfProps = {
     firebase,
     config: firebaseConfig,
     dispatch: store.dispatch,
-    createFirestoreInstance
+    createFirestoreInstance,
+    userProfile: 'users', // where profiles are stored in database
+    presence: 'presence', // where list of online users is stored in database
+    sessions: 'sessions'
   };
+
+  function AuthIsLoaded({ children }) {
+
+    const auth = useSelector(state => state.firebase.auth)
+    if (!isLoaded(auth)) return <div>Loading Screen...</div>;
+
+    return children
+
+}
 
 ReactDOM.render(
   //<React.StrictMode>
     <Provider store={store}>
-     <ReactReduxFirebaseProvider {...rrfProps}> 
-      <App />
+     <ReactReduxFirebaseProvider {...rrfProps}>
+      <AuthIsLoaded> 
+       <App />
+      </AuthIsLoaded>
      </ReactReduxFirebaseProvider>
     </Provider>,
   //</React.StrictMode>,
