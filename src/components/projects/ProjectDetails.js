be6d341a2337532ca72ebc4e5dd5b18c851a3cdc
ibrahem <ibrahem.ahmed.ibrahem500@gmail.com>
2021-02-25 14:18:20 +0200

    added formated dates

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -2,6 +2,7 @@ import {connect} from 'react-redux'
 import {firestoreConnect} from 'react-redux-firebase'
 import {compose} from 'redux'
 import {Redirect} from 'react-router-dom'
+import moment from 'moment'
 
 const ProjectDetails = (props) => {
     const {project,auth} = props;
@@ -17,7 +18,7 @@ const ProjectDetails = (props) => {
 
                 <div className="card-action gret lighten-4 grey-text">
                     <div>Posted by {project.authorFirstName} {project.authorLastName}</div>
-                    <div>2nd sep</div>
+                    <div>{project.createdAt && moment(project.createdAt.toDate()).calendar()}</div>
                 </div>
                 
             </div>
